5f2d2d33dae6abdb0017e2d72a3b406e65746a04
yeonjae <duseh73@gmail.com>
2020-11-29 18:44:56 +0900

    add description jangMovie

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,30 +13,30 @@
         </header>        
         <div class="content">
           <div class="textBox" v-if="img1">
-            <h2><span>AndPick</span></h2>            
-            <p><b>AndPick</b>은 다양한 분야의 뉴스기사 및 기술블로그의 정보를 제공합니다.</p>
-            <p>구독 기능은 관심있는 뉴스, 기술 블로그를 피드에 추가하여 최신 정보를 확인합니다.</p>
-            <p>보드 기능은 구독하고 있는 채널의 기사를 저장하여 보관합니다.</p>
-            <p>마이페이지는 다양한 편집 기능을 통해 자신의 생각을 정리하여 보관합니다.</p>                        
-            <a href="#"><span class="mdi mdi-github"></span>Github</a>            
+            <h2><span>장씨배우</span></h2>            
+            <p><b>장씨배우</b>은 다양한 분야의 뉴스기사 및 기술블로그의 정보를 제공합니다.</p>
+            <p>- 평점을 활용한 추천 알고리즘으로 영화를 추천합니다.</p>
+            <p>- 날씨API를 활용하여 해당 지역의 날씨에 어울리는 장르를 추천합니다.</p>
+            <p>- 이상형 월드컵으로 좋아하는 배우가 출연한 영화를 추천합니다.</p>                        
+            <a href="https://nervous-swartz-5a0624.netlify.app"><span class="mdi mdi-github"></span>Github</a>            
           </div>
 
           <div class="textBox" v-if="img2">
             <h2><span>My Museum</span></h2>            
             <p><b>My Museum</b>은 온라인을 통해 개인에 맞는 전시회를 제공합니다.</p>
-            <p>작가, 화풍, 박물관 별로 작품을 감상할 수 있습니다.</p>
-            <p>개인 취향에 맞는 작품을 추천받을 수 있습니다.</p>
-            <p>좋아하는 작품을 저장하고 나만의 전시회를 만들 수 있습니다.</p>                        
+            <p>- 작가, 화풍, 박물관 별로 작품을 감상할 수 있습니다.</p>
+            <p>- 개인 취향에 맞는 작품을 추천받을 수 있습니다.</p>
+            <p>- 좋아하는 작품을 저장하고 나만의 전시회를 만들 수 있습니다.</p>                        
             <a href="#"><span class="mdi mdi-github"></span>Github</a>            
           </div>
 
           <div class="textBox" v-if="img3">
             <h2><span>Byeper</span></h2>            
             <p><b>Byeper</b>는 학습자를 위한 교육 플랫폼입니다.</p>
-            <p>강의에서 변화율을 감지하여 자동으로 PPT를 추출하여 수업자료를 제공합니다.</p>            
-            <p>Youtube의 교육 유튜버가 강사가 되어 강의를 제공합니다.</p>
-            <p>동시적으로 정리를 할 수 있도록 노트 필기 기능 등 다양한 기능을 제공합니다.</p>
-            <p>인공지능을 활용하여 문제를 자동 생성합니다.</p>                        
+            <p>- 강의에서 변화율을 감지하여 자동으로 PPT를 추출하여 수업자료를 제공합니다.</p>            
+            <p>- Youtube의 교육 유튜버가 강사가 되어 강의를 제공합니다.</p>
+            <p>- 동시적으로 정리를 할 수 있도록 노트 필기 기능 등 다양한 기능을 제공합니다.</p>
+            <p>- NLTK를 활용하여 문제를 자동 생성합니다.</p>                        
             <a href="#"><span class="mdi mdi-github"></span>Github</a>            
           </div>
 
@@ -48,8 +48,9 @@
         </div>
         <ul class="thumb">
           <li><div @click="imgSlider(true, false, false, '#017143')" style="background-color: #017143;">1</div></li>
-          <li><div @click="imgSlider(false, true, false, '#eb7495')" style="background-color: #eb7495;">2</div></li>
-          <li><div @click="imgSlider(false, false, true, '#d752b1')" style="background-color: #d752b1;">3</div></li>
+          <li><div @click="imgSlider(true, false, false, '#017143')" style="background-color: #017143;">2</div></li>
+          <li><div @click="imgSlider(false, true, false, '#eb7495')" style="background-color: #eb7495;">3</div></li>
+          <li><div @click="imgSlider(false, false, true, '#d752b1')" style="background-color: #d752b1;">4</div></li>
         </ul>
         <ul class="sci">
           <li><a href="#"><img src="@/assets/facebook.png" alt=""></a></li>
@@ -68,6 +69,7 @@ export default {
       img1: true,
       img2: false,
       img3: false,
+      img4: false,
     }
   },
   methods: {
